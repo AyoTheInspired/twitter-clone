6baf00e3740df5a69b25f609353aea96167af86a
Ayo <bimboladxtreme@gmail.com>
2021-07-30 22:12:45 +0100

    Stylings on Feed component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
 		<div className="App">
-			<Sidebar />
+			{/* <Sidebar /> */}
+			<Feed />
 		</div>
 	);
 }
