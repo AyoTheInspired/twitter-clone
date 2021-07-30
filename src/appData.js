9f534e585b78185b598a59adb4f3321e186606d3
Ayo <bimboladxtreme@gmail.com>
2021-07-30 20:25:18 +0100

    Initial setup

---
+++ src/appData.js
@@ -0,0 +1,26 @@
+import {
+	FaSearch,
+	FaRegBell,
+	FaCommentDots,
+	FaPager,
+	FaBookReader,
+	FaRegHeart,
+	FaUserCheck,
+	FaMapMarkerAlt,
+	FaRegCalendarAlt,
+	FaUserFriends,
+} from "react-icons/fa";
+import { ImEqualizer2 } from "react-icons/im";
+import {
+	AiOutlineAppstore,
+	AiOutlineBars,
+	AiOutlinePieChart,
+	AiOutlineFundProjectionScreen,
+} from "react-icons/ai";
+import {
+	FiCheckCircle,
+	FiShoppingCart,
+	FiDollarSign,
+	FiShoppingBag,
+} from "react-icons/fi";
+import { RiPulseLine } from "react-icons/ri";
