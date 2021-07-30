9d889798819bc2c07337990ff0aff97cd097029a
Ayo <bimboladxtreme@gmail.com>
2021-07-30 21:10:13 +0100

    Added icons to SidebarOption component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-	return <div className="App">HELLO WORLD </div>;
+	return (
+		<div className="App">
+			<Sidebar />
+		</div>
+	);
 }
 
 export default App;
