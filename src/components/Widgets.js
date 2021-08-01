d78b17fd22ceee883724fd281f85403c06ddf679
Ayo <bimboladxtreme@gmail.com>
2021-08-01 18:55:10 +0100

    Added Twitter embed package

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,25 @@
+import Search from "@material-ui/icons/Search";
 import React from "react";
+import {
+	TwitterTimelineEmbed,
+	TwitterShareButton,
+	TwitterTweetEmbed,
+} from "react-twitter-embed";
 import "./Widgets.css";
 
 function Widgets() {
 	return (
 		<div className="widgets">
-			<h2>Widgets</h2>
+			<div className="widgets__input">
+				<Search className="widgets__searchIcon" />
+				<input type="text" placeholder="Search Twitter" />
+			</div>
+
+			<div className="widgets__widged">
+				<h2>What's Happening</h2>
+
+				<TwitterTweetEmbed tweetId={"858551177860055040"} />
+			</div>
 		</div>
 	);
 }
