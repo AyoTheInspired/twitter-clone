bd6b28b9a45e36d784b8770b563b0305e549f381
Ayo <bimboladxtreme@gmail.com>
2021-08-05 23:40:27 +0100

    Initialized firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAeHHy2_csFe3xxnRZYgMdgZD-MXwui6xs",
+	authDomain: "twitter-clone-d0964.firebaseapp.com",
+	projectId: "twitter-clone-d0964",
+	storageBucket: "twitter-clone-d0964.appspot.com",
+	messagingSenderId: "861500914871",
+	appId: "1:861500914871:web:98fae0fee8b82d82d2a132",
+	measurementId: "G-PVSDP4L3TH",
+};
