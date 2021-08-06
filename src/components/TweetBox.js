abb5b750fedf3ddde3772c8bd3478c39748313f8
Ayo <bimboladxtreme@gmail.com>
2021-08-06 02:20:55 +0100

    Installed React flip move

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,20 +1,49 @@
+import React, { useState } from "react";
 import { Button, Avatar } from "@material-ui/core";
-import React from "react";
 import "./TweetBox.css";
+import db from "../firebase";
 
 function TweetBox() {
+	const [tweetMessage, setTweetMessage] = useState("");
+	const [tweetImage, setTweetImage] = useState("");
+
+	const sendTweet = (e) => {
+		e.preventDefault();
+
+		db.collection("posts").add({
+			displayName: "Ayo Abimbola",
+			username: "ayotheinspired",
+			verified: true,
+			text: tweetMessage,
+			avatar:
+				"https://res.cloudinary.com/ayotheinspired/image/upload/v1620704711/inspired-global-media/images/Team%20Members/The_Inspired_qo4e4p.jpg",
+			image: "/images/chris.jpg",
+		});
+
+		setTweetMessage("");
+		setTweetImage("");
+	};
+
 	return (
 		<div className="tweetBox">
 			<form>
 				<div className="tweetBox__input">
 					<Avatar src="/images/user.jpg" />
-					<input placeholder="What's happening?" />
+					<input
+						value={tweetMessage}
+						onChange={(e) => setTweetMessage(e.target.value)}
+						placeholder="What's happening?"
+					/>
 				</div>
 				<input
-					placeholder="Optional: Enter an Image URL"
+					value={tweetImage}
+					onChange={(e) => setTweetImage(e.target.value)}
+					placeholder="NB: All tweets will render with static images."
 					className="tweetBox__imageInput"
 				/>
-				<Button className="tweetBox__tweetButton">Tweet</Button>
+				<Button onClick={sendTweet} className="tweetBox__tweetButton">
+					Tweet
+				</Button>
 			</form>
 		</div>
 	);
