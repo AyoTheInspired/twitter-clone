c77be932ed7e0d5ec4ff7049fc28c66e08d08f53
Ayo <bimboladxtreme@gmail.com>
2021-08-06 01:55:40 +0100

    Firebase is configured!

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
+import db from "../firebase";
 
 function Feed() {
 	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		db.collection("posts").onSnapshot((snapshot) =>
+			setPosts(snapshot.docs.map((doc) => doc.data()))
+		);
+	}, []);
 
 	return (
 		<div className="feed">
@@ -12,14 +19,18 @@ function Feed() {
 				<h2>HOME</h2>
 			</div>
 			<TweetBox />
-			<Post
-				displayName="Ayo Abimbola"
-				username="ayotheinspired"
-				verified
-				text="The Very First Test Tweet"
-				avatar="/images/user.jpg"
-				image="/images/stacie.jpg"
-			/>
+
+			{posts.map((post) => (
+				<Post
+					displayName={post.displayName}
+					username={post.username}
+					verified={post.verified}
+					text={post.text}
+					avatar={post.avatar}
+					image={post.image}
+				/>
+			))}
+
 			{/* <Post />
 			<Post />
 			<Post /> */}
