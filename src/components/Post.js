9dc65429f5b27cd5ea569be37f0e44818d1d6cad
Ayo <bimboladxtreme@gmail.com>
2021-08-06 02:36:35 +0100

    App is live!!

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import {
@@ -9,47 +9,44 @@ import {
 	VerifiedUser,
 } from "@material-ui/icons";
 
-function Post({
-	displayName,
-	username,
-	verified,
-	timestamp,
-	text,
-	image,
-	avatar,
-}) {
-	return (
-		<div className="post">
-			<div className="post__avatar">
-				<Avatar src={avatar} />
-			</div>
+const Post = forwardRef(
+	(
+		{ displayName, username, verified, timestamp, text, image, avatar },
+		ref
+	) => {
+		return (
+			<div className="post" ref={ref}>
+				<div className="post__avatar">
+					<Avatar src={avatar} />
+				</div>
 
-			<div className="post__body">
-				<div className="post__header">
-					<div className="post__headerText">
-						<h3>
-							{displayName}&nbsp;
-							<span className="post__headerSpecial">
-								{verified && <VerifiedUser className="post__badge" />} @
-								{username}
-							</span>
-						</h3>
+				<div className="post__body">
+					<div className="post__header">
+						<div className="post__headerText">
+							<h3>
+								{displayName}&nbsp;
+								<span className="post__headerSpecial">
+									{verified && <VerifiedUser className="post__badge" />} @
+									{username}
+								</span>
+							</h3>
+						</div>
+						<div className="post__headerDescription">
+							<p> {text} </p>
+						</div>
 					</div>
-					<div className="post__headerDescription">
-						<p> {text} </p>
+					<img src={image} width="400" alt="" />
+
+					<div className="post__footer">
+						<ChatBubbleOutline fontSize="small" />
+						<Repeat fontSize="small" />
+						<FavoriteBorder fontSize="small" />
+						<Publish fontSize="small" />
 					</div>
 				</div>
-				<img src={image} width="400" alt="" />
-
-				<div className="post__footer">
-					<ChatBubbleOutline fontSize="small" />
-					<Repeat fontSize="small" />
-					<FavoriteBorder fontSize="small" />
-					<Publish fontSize="small" />
-				</div>
 			</div>
-		</div>
-	);
-}
+		);
+	}
+);
 
 export default Post;
